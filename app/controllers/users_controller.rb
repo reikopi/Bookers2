c3b40bdee916f04834adf668e5e47d66d2140fd8
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-02 00:49:04 +0000

    [Add]4-bookers2_User_showおよびeditの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user =　User.find(params[:id])
+    @books = @user.books.page(params[:page])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
