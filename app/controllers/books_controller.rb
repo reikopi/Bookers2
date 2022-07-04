c0c401bc0ddebeddbb76deae2d552fefbe9f798f
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-04 13:57:08 +0000

    [Add]4-bookers2_update,destroy,footerの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,18 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
+   @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to books_path
   end
 
   def index
-   @books = Books.all
+   @book = Book.all
   end
 
   def show
@@ -21,13 +21,25 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(@book), method: :show
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 
    # 投稿データのストロングパラメータ
   private
 
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.require(:book).permit(:title,:caption)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   get '/about' => 'homes#about'
   devise_for :users
-  
-  resources :books, only: [:new, :create, :index, :show]
+
+  resources :books, only: [:new, :create, :index, :show, :update, :destroy]
   resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
