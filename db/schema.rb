be855911f8b5987922723745fd6236efa8cdebd2
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-19 11:08:28 +0000

    [修正]4-bookers2_カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_024149) do
+ActiveRecord::Schema.define(version: 2022_06_26_014335) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
